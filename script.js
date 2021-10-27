2a1ad419b3ffc8494070e139d2dd74ca363d11f8
Indero <70791323+InderoFU@users.noreply.github.com>
2021-10-26 20:53:12 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function twitterFunc() {
+    window.location.replace("https://www.twitter.com/InderoFU");
+} 
+    
+function instagramFunc() {
+    window.location.replace("https://www.instagram.com/InderoFU");
+} 
+
+function youtubeFunc() {
+    window.location.replace("https://www.youtube.com/channel/UCNt37CLMZNT8F_dsqTkRdWw");
+}
+
+function emailFunc() {
+    window.location.replace("emailto:darkindero@gmail.com");
+}
+
+function githubFunc() {
+    window.location.replace("https://github.com/InderoFU");
+}
