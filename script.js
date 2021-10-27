2ebcfa44558d13ca2b5283c2f58f2a5fafa19a88
Indero <70791323+InderoFU@users.noreply.github.com>
2021-10-26 20:55:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,6 @@ function youtubeFunc() {
     window.location.replace("https://www.youtube.com/channel/UCNt37CLMZNT8F_dsqTkRdWw");
 }
 
-function emailFunc() {
-    window.location.replace("emailto:darkindero@gmail.com");
-}
-
 function githubFunc() {
     window.location.replace("https://github.com/InderoFU");
 }
