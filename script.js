256acf8d645817dcf5a0f874cd9152eac35fc59e
Indero <70791323+InderoFU@users.noreply.github.com>
2021-11-04 08:24:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 // JS buttons that open a new window for the button that is pressed
 
 function twitterFunc() {
-    window.open("https://www.twitter.com/InderoFU");
+  window.open("https://www.twitter.com/InderoFU");
 } 
     
 function instagramFunc() {
-    window.open("https://www.instagram.com/InderoFU");
+  window.open("https://www.instagram.com/InderoFU");
 } 
 
 function youtubeFunc() {
-    window.open("https://www.youtube.com/channel/UCNt37CLMZNT8F_dsqTkRdWw");
+  window.open("https://www.youtube.com/channel/UCNt37CLMZNT8F_dsqTkRdWw");
 }
 
 function githubFunc() {
-    window.open("https://github.com/InderoFU");
+  window.open("https://github.com/InderoFU");
 }
 
 function discordFunc() {
   window.open("https://discord.gg/FgjdxHDFCb")
+}
+
+function aboutMe() {
+  window.location.replace("About Me.html")
+}
+
+function main() {
+  window.location.replace("index.html")
 }
